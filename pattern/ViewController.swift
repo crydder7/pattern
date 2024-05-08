7e8083caf5f401ffce58b143456e1154e041f867
crydder7 <getright1337ez@gmail.com>
2024-05-08 18:12:56 +0300

    changes

--- pattern/ViewController.swift
+++ pattern/ViewController.swift
@@ -1,19 +1,43 @@
-//
-//  ViewController.swift
-//  pattern
-//
-//  Created by lonely. on 08.05.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    let button = UIButton()
+    let imgView = UIImageView()
+    let label = UILabel()
+    
+    let au = UIImage(named: "spbau.jpg")
+    let polit = UIImage(named: "spbstu.jpg")
+    let su = UIImage(named: "spbsu.jpg")
+    let itmo = UIImage(named: "itmo.png")
+    let suai = UIImage(named: "suai.jpg")
+    let leti = UIImage(named: "leti.jpg")
+    let spbsti = UIImage(named: "spbsti.jpg")
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        displayButton()
+        view.addSubview(imgView)
+        view.addSubview(label)
     }
 
-
+    func displayButton(){
+        button.frame = CGRect(x: 0, y: 650, width: 200, height: 40)
+        button.center.x = view.center.x
+        button.setTitle("Random", for: .normal)
+        button.backgroundColor = .darkGray
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 5
+        button.layer.borderColor = .init(red: 0, green: 0, blue: 0, alpha: 1)
+        button.addTarget(self, action: #selector(dispImage(sender: )), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    
+    @objc func dispImage(sender:Any){
+        let imgs = [au! :"СПБАУ",polit! :"Политех",su! :"СПБГУ",itmo!:"ИТМО",suai!:"ГУАП",spbsti!:"СПБГТИ",leti!:"ЛЭТИ"]
+        let rndPick = imagePicker(imgs: imgs, imgView: imgView, label: label, viewCon: self)
+        rndPick.display()
+    }
 }
 
--- pattern/myClass.swift
+++ pattern/myClass.swift
@@ -1,8 +1,33 @@
-//
-//  myClass.swift
-//  pattern
-//
-//  Created by lonely. on 08.05.2024.
-//
-
 import Foundation
+import UIKit
+
+class imagePicker{
+    let imgs : [UIImage:String]
+    let imgView : UIImageView
+    let label : UILabel
+    let viewCon : UIViewController
+    init(imgs: [UIImage : String], imgView: UIImageView, label: UILabel, viewCon: UIViewController) {
+        self.imgs = imgs
+        self.imgView = imgView
+        self.label = label
+        self.viewCon = viewCon
+    }
+    
+    func display(){
+        imgView.frame = CGRect(x: 0, y: 0, width: 350, height: 350)
+        imgView.center = viewCon.view.center
+        label.frame = CGRect(x: 0, y: 50, width: 150, height: 35)
+        label.textAlignment = .center
+        label.layer.borderColor = .init(red: 0, green: 0, blue: 0, alpha: 1)
+        label.layer.borderWidth = 5
+        label.layer.cornerRadius = 15
+        label.font = .systemFont(ofSize: 20)
+        label.textColor = .black
+        label.center.x = viewCon.view.center.x
+        let picToUnwrap = imgs.keys.randomElement()
+        guard let rndPic = picToUnwrap else {return}
+        let rndText = imgs[rndPic]
+        imgView.image = rndPic
+        label.text = rndText
+    }
+}
