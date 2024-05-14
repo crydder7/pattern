a428cbf4b9a052b0d5b82b63f9435bb5781dcca8
crydder7 <getright1337ez@gmail.com>
2024-05-14 22:51:38 +0300

    Added class(more flexability)

--- pattern/myClass.swift
+++ pattern/myClass.swift
@@ -3,19 +3,46 @@ import UIKit
 
 class imagePicker{
     let imgs : [UIImage:String]
+    var universities : [University]
     let imgView : UIImageView
     let label : UILabel
     let viewCon : UIViewController
-    init(imgs: [UIImage : String], imgView: UIImageView, label: UILabel, viewCon: UIViewController) {
+    let size : CGFloat
+    let xCoordinate : CGFloat
+    let yCoordinate : CGFloat
+    let infoButton : UIButton
+    
+    init(imgs: [UIImage : String], imgView: UIImageView, label: UILabel, viewCon: UIViewController, size : CGFloat, xCoordinate: CGFloat, yCoordinate: CGFloat, infoButton: UIButton) {
         self.imgs = imgs
         self.imgView = imgView
         self.label = label
         self.viewCon = viewCon
+        self.size = size
+        self.xCoordinate = xCoordinate
+        self.yCoordinate = yCoordinate
+        self.infoButton = infoButton
+        self.universities = []
+        for i in self.imgs.keys{
+            self.universities.append(University(name: self.imgs[i]!, logo: i))
+        }
     }
     
-    func display(){
-        imgView.frame = CGRect(x: 0, y: 0, width: 350, height: 350)
-        imgView.center = viewCon.view.center
+    func display()->String{
+        let uni = universities.randomElement()
+        let pic = uni?.logo
+        let name = uni?.name
+//        let picToUnwrap = imgs.keys.randomElement()
+//        guard let rndPic = picToUnwrap else {return}
+//        let rndText = imgs[rndPic]
+        imgView.image = pic
+        label.text = name
+        return uni!.description
+    }
+    
+    func setupImg(){
+        imgView.frame = CGRect(x: 0, y: 0, width: size, height: size)
+        imgView.center.x = xCoordinate
+        imgView.center.y = yCoordinate
         imgView.layer.shadowOffset = CGSize(width: 10, height: 15)
         imgView.layer.shadowColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
         imgView.layer.shadowOpacity = 0.5
@@ -30,10 +57,17 @@ class imagePicker{
         label.layer.shadowColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
         label.layer.shadowOpacity = 0.5
         label.center.x = viewCon.view.center.x
-        let picToUnwrap = imgs.keys.randomElement()
-        guard let rndPic = picToUnwrap else {return}
-        let rndText = imgs[rndPic]
-        imgView.image = rndPic
-        label.text = rndText
+    }
+}
+
+
+class University{
+    let name : String
+    let logo : UIImage
+    lazy var description = "This is \(name)"
+    
+    init(name: String, logo: UIImage) {
+        self.name = name
+        self.logo = logo
     }
 }
--- pattern/pictures.swift
+++ pattern/pictures.swift
@@ -11,15 +11,36 @@ class pictures: UIViewController {
     let suai = UIImage(named: "suai.png")
     let leti = UIImage(named: "leti.png")
     let spbsti = UIImage(named: "spbsti.png")
+    let infoButton = UIButton(type: .infoDark)
+    var msg = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
         let imgs = [au! :"СПБАУ",polit! :"Политех",su! :"СПБГУ",itmo!:"ИТМО",suai!:"ГУАП",spbsti!:"СПБГТИ",leti!:"ЛЭТИ"]
-        let rndPick = imagePicker(imgs: imgs, imgView: imgView, label: label, viewCon: self)
-        rndPick.display()
+        let rndPick = imagePicker(imgs: imgs, imgView: imgView, label: label, viewCon: self, size: 350, xCoordinate: view.center.x, yCoordinate: view.center.y, infoButton: infoButton)
+        rndPick.setupImg()
+        msg = rndPick.display()
+        infoButtonDisp()
         view.addSubview(imgView)
         view.addSubview(label)
+    }
+    
+    func infoButtonDisp(){
+        infoButton.frame = CGRect(x: label.center.x + 150, y: label.center.y - 10, width: 25, height: 25)
+        infoButton.layer.shadowOffset = CGSize(width: 10, height: 10)
+        infoButton.layer.shadowColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
+        infoButton.layer.shadowOpacity = 0.5
+        infoButton.tintColor = .black
+        view.addSubview(infoButton)
+        infoButton.addTarget(self, action: #selector(showInfo(sender:)), for: .touchUpInside)
+    }
+    
+    @objc func showInfo(sender:UIButton){
+        let alert = UIAlertController(title: msg, message: nil, preferredStyle: .actionSheet)
+        let action = UIAlertAction(title: "Great", style: .default)
+        alert.addAction(action)
+        self.present(alert, animated: true)
     }
 
 }
